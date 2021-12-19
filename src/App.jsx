600002c647d1d8fa0eea12d6d8de6bac582e9b94
Luiz Lima <umluizlima@gmail.com>
2021-12-19 20:33:47 -0300

    Change DAO name

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to SerendipityDAO</h1>
         <button onClick={() => connectWallet("injected")} className="btn-hero">
           Connect your wallet
         </button>
