259b14e561d21d06124b0cddccbe59cceb106650
Luiz Lima <umluizlima@gmail.com>
2021-12-19 21:08:03 -0300

    Add deploy drop script

---
+++ scripts/2-deploy-drop.js
@@ -0,0 +1,34 @@
+import { ethers } from "ethers";
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+const app = sdk.getAppModule(process.env.THIRDWEB_APP_ADDRESS);
+
+(async () => {
+  try {
+    const bundleDropModule = await app.deployBundleDropModule({
+      // The collection's name, ex. CryptoPunks
+      name: "SerendipityDAO Membership",
+      // A description for the collection.
+      description: "A DAO for promoting beneficial connections.",
+      // The image for the collection that will show up on OpenSea.
+      image: readFileSync("scripts/assets/clover.png"),
+      // We need to pass in the address of the person who will be receiving the proceeds from sales of nfts in the module.
+      // We're planning on not charging people for the drop, so we'll pass in the 0x0 address
+      // you can set this to your own wallet address if you want to charge for the drop.
+      primarySaleRecipientAddress: ethers.constants.AddressZero,
+      // primarySaleRecipientAddress: process.env.WALLET_ADDRESS,
+    });
+    
+    console.log(
+      "✅ Successfully deployed bundleDrop module, address:",
+      bundleDropModule.address,
+    );
+    console.log(
+      "✅ bundleDrop metadata:",
+      await bundleDropModule.getMetadata(),
+    );
+  } catch (error) {
+    console.log("failed to deploy bundleDrop module", error);
+  }
+})();
